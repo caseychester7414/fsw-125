114493bfda33c5fa6ec81932e45dc95ff9afc252
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-06-03 12:40:01 -0500

    math

---
+++ week1/math/math.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+return a + b
+}
+function multi(a,b){
+return a * b
+}
+function divide(a,b){
+return a / b
+}
+function sub(a,b){
+return a - b
+}
+                    
+
+module.exports = {sum,multi,divide,sub}
